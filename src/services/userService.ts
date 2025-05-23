55a6986e520f6832e5678bfc1f4d5c49f32023c6
Anishshet <anishshet5@gmail.com>
2025-05-23 12:23:26 +0530

    env and edit cutton commited (useless )

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,32 +1,25 @@
-import axios from "axios"
+import axios from "axios";
 import type { LoginCredentials } from "../types/user";
 
-
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL,
-//   });
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const userService = {
 
-  async getUsers(){
-    const token = sessionStorage.getItem('access_token')
-      const res = await fetch("https://8e19-2401-4900-1c7e-ad2-5504-7bb7-3b95-bf19.ngrok-free.app/api/user/USER01",
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      });
-      console.log(res)
-    },
+  async getUsers() {
+    const token = sessionStorage.getItem('access_token');
+    const res = await fetch(`${API_BASE_URL}/user/USER01`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    console.log(res);
+    return res;
+  },
 
- async login(credentials:LoginCredentials){
-    const res = await axios.post("https://90ca-2401-4900-1c7e-ad2-5504-7bb7-3b95-bf19.ngrok-free.app/api/login",credentials)
-    return res
-}
-
-
-    
-}
+  async login(credentials: LoginCredentials) {
+    const res = await axios.post(`${API_BASE_URL}/login`, credentials);
+    return res;
+  }
+};
